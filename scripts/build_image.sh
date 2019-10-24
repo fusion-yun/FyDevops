165b41ed5e106efbd018d209892f5c9d2196626b
yuzhi <yuzhi@ipp.ac.cn>
2019-10-24 17:56:06 +0800

    add image build script

---
+++ scripts/build_image.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+IMG_NAME=imas_3_24_0_ual_4_2_0
+docker build --progress=plain --ssh=default --rm -f "dockerfiles/imas.dockerfile" -t imas:3_24_0_ual_4_2_0 . >> build_${IMG_NAME}.log 2>&1  & # redirect stderr to log file
+echo $! >build_${IMG_NAME}.pid # save pid
