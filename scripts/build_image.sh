6d3ad45d798be25d74c2498a06caea782b358ad5
yuzhi <yuzhi@ipp.ac.cn>
2019-10-24 19:38:01 +0800

    clean

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -2,3 +2,8 @@
 IMG_NAME=imas_3_24_0_ual_4_2_0
 docker build --progress=plain --ssh=default --rm -f "dockerfiles/imas.dockerfile" -t imas:3_24_0_ual_4_2_0 . >> build_${IMG_NAME}.log 2>&1  & # redirect stderr to log file
 echo $! >build_${IMG_NAME}.pid # save pid
+
+
+###
+# save image to tar.gz
+docker save myimage:latest | gzip > myimage_latest.tar.gz
