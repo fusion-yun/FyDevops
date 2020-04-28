6d8f36c460cd7d7832bf691bd1b038f73a074fbb
yuzhi <yuzhi@ipp.ac.cn>
2020-04-28 09:14:10 +0800

    snapshot

--- dockerfiles/fylab.dockerfile
+++ dockerfiles/fylab.dockerfile
@@ -15,35 +15,45 @@ ARG FYLAB_VERSION=${FYLAB_VERSION:-0.0.0}
 ENV FYLAB_VERSION=${FYLAB_VERSION}
 
 USER   ${FYDEV_USER}
+ARG HOME_DIR=/home/${FYDEV_USER}
 ####################################################################
 # install packages
 
+RUN --mount=type=cache,uid=1000,id=fycache,target=/tmp/cache,sharing=shared \
+    if ! [ -z ${FUYUN_DIR}/share/fonts/NotoSansCJKsc-Regular.otf ] ; then \    
+    mkdir -p ${HOME_DIR}/.local/share/fonts/ ; \
+    cd ${HOME_DIR}/.local/share/fonts/ ; \
+    curl -LO https://github.com/googlefonts/noto-cjk/raw/master/NotoSansCJKsc-Regular.otf ; \
+    curl -LO https://github.com/googlefonts/noto-cjk/raw/master/NotoSansCJKsc-Bold.otf  ; \    
+    fi ; \
+    source /etc/profile.d/modules.sh ;\    
+    module load fontconfig ;\
+    fc-cache -fv 
+
 ARG TOOLCHAIN_NAME=${TOOLCHAIN_NAME:-foss}
 ARG TOOLCHAIN_VERSION=${TOOLCHAIN_VERSION:-2019b}
-
-
 ENV EASYBUILD_PREFIX=${FUYUN_DIR} 
+
 
 ################################################################################
 # For release
-
 RUN --mount=type=bind,target=/tmp/ebfiles,source=./ \
     source /etc/profile.d/modules.sh ;\    
     module avail ; \
     module load EasyBuild ; \   
-    eb   -r  --use-existing-modules --minimal-toolchain \
+    eb   -lr  --use-existing-modules --minimal-toolchain \
     --robot-paths=/tmp/ebfiles:$EBROOTEASYBUILD/easybuild/easyconfigs  \
     JupyterLab-2.1.1-foss-2019b-Python-3.7.4.eb
 
 
-# RUN --mount=type=bind,target=/tmp/ebfiles,source=./ \
-#     source /etc/profile.d/modules.sh ;\    
-#     module load EasyBuild ; \   
-#     export PYPI_MIRROR=https://pypi.tuna.tsinghua.edu.cn/simple ;\
-#     eb   -r --rebuild  --use-existing-modules --minimal-toolchain \
-#     --robot-paths=/tmp/ebfiles:$EBROOTEASYBUILD/easybuild/easyconfigs  \
-#     --moduleclasses=fuyun \
-#     FyLab-${FYLAB_VERSION}.eb  
+RUN --mount=type=bind,target=/tmp/ebfiles,source=./ \
+    source /etc/profile.d/modules.sh ;\    
+    module load EasyBuild ; \   
+    export PYPI_MIRROR=https://pypi.tuna.tsinghua.edu.cn/simple ;\
+    eb   -r --rebuild  --use-existing-modules --minimal-toolchain \
+    --robot-paths=/tmp/ebfiles:$EBROOTEASYBUILD/easybuild/easyconfigs  \
+    --moduleclasses=fuyun \
+    FyLab-${FYLAB_VERSION}.eb  
 
 # eb  --info  --use-existing-modules  --minimal-toolchain --robot-paths=/workspaces/FyDevOps/ebfiles/:$EBROOTEASYBUILD/easybuild/easyconfigs  --moduleclasses=fuyun /workspaces/FyDevOps/ebfiles/FyLab-0.0.0.eb -Dr
 
--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -16,59 +16,38 @@ FYLAB_VERSION=0.0.0
 #      --build-arg FY_OS=${FY_OS} \
 #      --build-arg FY_OS_VERSION=${FY_OS_VERSION} \
 #      -t fybase:${IMAGE_TAG} \
-<<<<<<< HEAD
 #      -f ../dockerfiles/fyBase.${FY_OS}.${FY_OS_VERSION}.dockerfile   \
 #      ../ebfiles
 
-=======
-#      - < ../dockerfiles/fyBase.${FY_OS}.${FY_OS_VERSION}.dockerfile   
-#
->>>>>>> bb7e8fb14018a8cdad8046b7b08248e8a85affdf
 # docker tag fybase:${IMAGE_TAG} fybase:latest
 
 
-echo "=======  Build FyDev" $(date +"%Y%m%d") " [" $(date) "] ============ "
-docker build --progress=plain  --rm \
-     --build-arg FY_OS=${FY_OS}  \
-     --build-arg FY_OS_VERSION=${FY_OS_VERSION} \
-     --build-arg TOOLCHAIN_NAME=${TOOLCHAIN_NAME} \
-     --build-arg TOOLCHAIN_VERSION=${TOOLCHAIN_VERSION} \
-     --build-arg FYDEV_VERSION=${FYDEV_VERSION} \
-<<<<<<< HEAD
-     --build-arg BASE_TAG=fybase:latest \
-=======
-     --build-arg IMAGE_TAG=${IMAGE_TAG} \
->>>>>>> bb7e8fb14018a8cdad8046b7b08248e8a85affdf
-     -t fydev:${IMAGE_TAG} \
-     -f ../dockerfiles/fydev.dockerfile \
-     ../ebfiles     
-
-<<<<<<< HEAD
-# docker tag fydev:${IMAGE_TAG} fydev:latest
-
-# echo "=======  Build FyLab" $(date +"%Y%m%d") " [" $(date) "] ============ "
+# echo "=======  Build FyDev" $(date +"%Y%m%d") " [" $(date) "] ============ "
 # docker build --progress=plain  --rm \
-#      --build-arg BASE_TAG=fydev:latest \
-=======
-docker tag fydev:${IMAGE_TAG} fydev:latest
-
-# echo "=======  Build FyLab" $(date +"%Y%m%d") " [" $(date) "] ============ "
-# docker build --progress=plain  --rm \
-#      --build-arg IMAGE_TAG=${IMAGE_TAG} \
->>>>>>> bb7e8fb14018a8cdad8046b7b08248e8a85affdf
+#      --build-arg FY_OS=${FY_OS}  \
+#      --build-arg FY_OS_VERSION=${FY_OS_VERSION} \
 #      --build-arg TOOLCHAIN_NAME=${TOOLCHAIN_NAME} \
 #      --build-arg TOOLCHAIN_VERSION=${TOOLCHAIN_VERSION} \
-#      --build-arg FYLAB_VERSION=${FYLAB_VERSION} \
-#      -t fylab:${IMAGE_TAG} \
-#      -f ../dockerfiles/fylab.dockerfile \
-#      ../ebfiles
+#      --build-arg FYDEV_VERSION=${FYDEV_VERSION} \
+#      --build-arg IMAGE_TAG=${IMAGE_TAG} \
+#      -t fydev:${IMAGE_TAG} \
+#      -f ../dockerfiles/fydev.dockerfile \
+#      ../ebfiles     
 
-# docker tag fylab:${IMAGE_TAG} fylab:latest
+# docker tag fydev:${IMAGE_TAG} fydev:latest
 
-<<<<<<< HEAD
-# echo "======= Done [" $(date) "]============ "  
-=======
+echo "=======  Build FyLab" $(date +"%Y%m%d") " [" $(date) "] ============ "
+docker build --progress=plain  --rm \
+     --build-arg IMAGE_TAG=${IMAGE_TAG} \
+     --build-arg TOOLCHAIN_NAME=${TOOLCHAIN_NAME} \
+     --build-arg TOOLCHAIN_VERSION=${TOOLCHAIN_VERSION} \
+     --build-arg FYLAB_VERSION=${FYLAB_VERSION} \
+     -t fylab:${IMAGE_TAG} \
+     -f ../dockerfiles/fylab.dockerfile \
+     ../ebfiles
+
+docker tag fylab:${IMAGE_TAG} fylab:latest
+
 echo "======= Done [" $(date) "]============ "  
->>>>>>> bb7e8fb14018a8cdad8046b7b08248e8a85affdf
 
 #docker run --rm -it --mount source=/home/salmon/workspace,target=/workspaces,type=bind fydev:latest
