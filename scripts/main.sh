9b823ae83b0b09661ff78e86b09da9dcebbd66e0
yuzhi <yuzhi@ipp.ac.cn>
2020-04-30 09:31:25 +0800

    fix bug in fydev.dockerfile

--- scripts/main.sh
+++ scripts/main.sh
@@ -4,4 +4,4 @@ BUILD_TAG=$(date +"%Y%m%d")_$(git describe --dirty --always --tags)
 
 `pwd`/build_images.sh > /tmp/build_fydev_${BUILD_TAG}.log 2>&1 & echo $! > /tmp/build_fydev_${BUILD_TAG}.pid
 
-echo "tag is " ${BUILD_TAG}
+echo "Build  " ${BUILD_TAG}
