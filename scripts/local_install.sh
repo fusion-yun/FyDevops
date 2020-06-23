96ca6ecee9f0a6d1c03d0f9ce4baf5a0ca6a8403
Salmon <yuzhi@ipp.ac.cn>
2020-06-24 04:41:19 +0800

    snapshot

--- scripts/local_install.sh
+++ scripts/local_install.sh
@@ -33,19 +33,23 @@ echo "=======  Build FyDev [" $(date +"%Y%m%d") ${SCRIPT_TAG} "] ============ "
 FY_EB_ARGS="--buildpath=${BUILD_DIR}/   \
   --sourcepath=${SOURCE_DIR} \
   --robot-paths=${EBFILES_PATH}:${EBROOTEASYBUILD}/easybuild/easyconfigs  \
-     --minimal-toolchain --use-existing-modules  -lr "
+  --minimal-toolchain --use-existing-modules  -lr "
 
 eb ${FY_EB_ARGS} --show-config  
 
-eb ${FY_EB_ARGS} GCCcore-8.3.0.eb   && \
-eb ${FY_EB_ARGS} Tcl-8.6.9-GCCcore-8.3.0.eb && \
-eb ${FY_EB_ARGS} Python-3.7.4-GCCcore-8.3.0.eb  && \
-eb ${FY_EB_ARGS} Perl-5.30.0-GCCcore-8.3.0.eb  PCRE-8.43-GCCcore-8.3.0.eb   && \
-eb ${FY_EB_ARGS} gompi-2019b.eb  && \
-eb ${FY_EB_ARGS} HDF5-1.10.5-gompi-2019b.eb  && \
-eb ${FY_EB_ARGS} Boost-1.71.0-gompi-2019b.eb  && \
-eb ${FY_EB_ARGS} foss-2019b.eb  && \
-eb ${FY_EB_ARGS} SciPy-bundle-2019.10-foss-2019b-Python-3.7.4.eb  && \
+# eb ${FY_EB_ARGS} GCCcore-8.3.0.eb   && \
+# eb ${FY_EB_ARGS} Tcl-8.6.9-GCCcore-8.3.0.eb && \
+# eb ${FY_EB_ARGS} Python-3.7.4-GCCcore-8.3.0.eb  && \
+# eb ${FY_EB_ARGS} Perl-5.30.0-GCCcore-8.3.0.eb  PCRE-8.43-GCCcore-8.3.0.eb   && \
+# eb ${FY_EB_ARGS} gompi-2019b.eb  && \
+# eb ${FY_EB_ARGS} HDF5-1.10.5-gompi-2019b.eb  && \
+# eb ${FY_EB_ARGS} Boost-1.71.0-gompi-2019b.eb  && \
+# eb ${FY_EB_ARGS} foss-2019b.eb  && \
+# eb ${FY_EB_ARGS} SciPy-bundle-2019.10-foss-2019b-Python-3.7.4.eb  && \
+# eb ${FY_EB_ARGS} netCDF-4.7.1-gompi-2019b.eb
+# eb ${FY_EB_ARGS} netCDF-C++4-4.3.1-gompi-2019b.eb
+eb ${FY_EB_ARGS} PyQt5-5.15.0-GCCcore-8.3.0-Python-3.7.4.eb
 eb ${FY_EB_ARGS} --moduleclasses=fuyun  FyDev-0.0.1.eb   
+eb ${FY_EB_ARGS} --moduleclasses=fuyun  FyLab-0.0.1.eb   
 
 echo "======= Done [" ${SCRIPT_TAG} "]============ "
--- scripts/local_install_imas.sh
+++ scripts/local_install_imas.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+#
+#  Build and install IMAS environment
+#
 
-# OS dependence
-# sudo yum install -y texlive texlive-epstopdf 
+FUYUN_HOME=/gpfs/fuyun
 
+SOURCE_DIR=${FUYUN_HOME}/sources
+BUILD_DIR=/tmp/fybuild
+mkdir -p ${BUILD_DIR}
 
-export EASYBUILD_PREFIX=/gpfs/fuyun 
-FY_EB_VERSION=${FY_EB_VERSION:-4.2.0}
+SCRIPT_DIR=$(cd "$(dirname "$0")/../"; pwd)
+SCRIPT_TAG=$(git describe --dirty --always --tags)
+ 
+export EASYBUILD_PREFIX=${FUYUN_HOME}
+ 
+#source /etc/profile.d/modules.sh 
 
 module use ${EASYBUILD_PREFIX}/modules/all
 module load EasyBuild/${FY_EB_VERSION} 
@@ -14,29 +23,35 @@ module load EasyBuild/${FY_EB_VERSION}
 TOOLCHAIN="foss-2019b"
 IMAS_VERSION="3.28.1_4.7.2"
 
-SCRIPT_DIR=$(cd "$(dirname "$0")/../"; pwd)
-SCRIPT_TAG=$(git describe --dirty --always --tags)
 
-TEMP_BUILD_DIR=/tmp/imasbuild_$(date +"%Y%m%d")
 
-mkdir -p ${TEMP_BUILD_DIR}
+EBFILES_PATH=${SCRIPT_DIR}/easybuild/easyconfigs:${SCRIPT_DIR}/../imas_ebs/easybuild/easyconfigs
 
-OTHER_EBFILES=${SCRIPT_DIR}/../FyDevOps/easybuild/easyconfigs/
+echo "=======  Build IMAS [" $(date +"%Y%m%d") ${SCRIPT_TAG} "] ============ "  
 
-echo "=======  Build IMAS  [" $(date +"%Y%m%d") ${SCRIPT_TAG} "] ============ "  
+FY_EB_ARGS="--buildpath=${BUILD_DIR}/   \
+  --sourcepath=${SOURCE_DIR} \
+  --robot-paths=${EBFILES_PATH}:${EBROOTEASYBUILD}/easybuild/easyconfigs  \
+  --minimal-toolchain --use-existing-modules  -lr "
 
-EB_ARGS="--buildpath=${TEMP_BUILD_DIR}/   \
-  --robot-paths=${OTHER_EBFILES}:${SCRIPT_DIR}/easybuild/easyconfigs:${EBROOTEASYBUILD}/easybuild/easyconfigs  \
-  --sourcepath=${SCRIPT_DIR}/sources:${EASYBUILD_PREFIX}/sources \
-  --skip-test-cases --minimal-toolchain --use-existing-modules  "
+eb ${FY_EB_ARGS} --show-config  
 
-eb  ${EB_ARGS} --show-config
-# eb  ${EB_ARGS} -lr  Python-3.7.4-GCCcore-8.3.0.eb 
-# eb  ${EB_ARGS} -lr  IMAS-${IMAS_VERSION}-${TOOLCHAIN}.eb 
-# eb  ${EB_ARGS} -lr  IMAS_devs-${IMAS_VERSION}-${TOOLCHAIN}.eb 
-# eb  ${EB_ARGS} -lr  FRUIT-3.4.3-GCCcore-8.3.0-Ruby-2.7.1.eb
-eb  ${EB_ARGS} -l --rebuild  XMLlib-3.3.0-GCCcore-8.3.0.eb FC2K-4.10.1-foss-2019b-IMAS-3.28.1_4.7.2.eb
-eb  ${EB_ARGS} -lr GENRAY-10.11.1-foss-2019b-IMAS-3.28.1_4.7.2.eb
-# eb  ${EB_ARGS} -lr  IMAS_actors-${IMAS_VERSION}-${TOOLCHAIN}.eb 
-# rm -rf ${TEMP_BUILD_DIR}
+eb ${FY_EB_ARGS} IMAS-3.28.1_4.7.2-foss-2019b.eb
+eb ${FY_EB_ARGS} IMAS_devs-3.28.1_4.7.2-foss-2019b.eb
+eb ${FY_EB_ARGS} IMAS_actors-3.28.1_4.7.2-foss-2019b.eb
+# eb ${FY_EB_ARGS} GCCcore-8.3.0.eb   && \
+# eb ${FY_EB_ARGS} Tcl-8.6.9-GCCcore-8.3.0.eb && \
+# eb ${FY_EB_ARGS} Python-3.7.4-GCCcore-8.3.0.eb  && \
+# eb ${FY_EB_ARGS} Perl-5.30.0-GCCcore-8.3.0.eb  PCRE-8.43-GCCcore-8.3.0.eb   && \
+# eb ${FY_EB_ARGS} gompi-2019b.eb  && \
+# eb ${FY_EB_ARGS} HDF5-1.10.5-gompi-2019b.eb  && \
+# eb ${FY_EB_ARGS} Boost-1.71.0-gompi-2019b.eb  && \
+# eb ${FY_EB_ARGS} foss-2019b.eb  && \
+# eb ${FY_EB_ARGS} SciPy-bundle-2019.10-foss-2019b-Python-3.7.4.eb  && \
+# eb ${FY_EB_ARGS} netCDF-4.7.1-gompi-2019b.eb
+# eb ${FY_EB_ARGS} netCDF-C++4-4.3.1-gompi-2019b.eb
+# eb ${FY_EB_ARGS} PyQt5-5.15.0-GCCcore-8.3.0-Python-3.7.4.eb
+# eb ${FY_EB_ARGS} --moduleclasses=fuyun  FyDev-0.0.1.eb   
+# eb ${FY_EB_ARGS} --moduleclasses=fuyun  FyLab-0.0.1.eb   
+
 echo "======= Done [" ${SCRIPT_TAG} "]============ "
